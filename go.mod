dd201c01bb25d0d607f273bed370f18f6317c5f1
Andrew Chambers <andrewchambers@fastmail.com>
2018-10-02 14:45:11 +1300

    Add connection manager type

--- go.mod
+++ go.mod
@@ -1,1 +1,6 @@
 module github.com/andrewchambers/go-extra
+
+require (
+	golang.org/x/net v0.0.0-20180926154720-4dfa2610cdf3 // indirect
+	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f
+)
---
+++ net/listener.go
@@ -0,0 +1,133 @@
+package net
+
+import (
+	"context"
+	"net"
+	"sync/atomic"
+	"time"
+
+	"github.com/andrewchambers/go-extra/errors"
+	"golang.org/x/sync/semaphore"
+)
+
+type ConnectionManagerOptions struct {
+	Domain        string
+	Addr          string
+	MaxConcurrent int64
+}
+
+func (l *ConnectionManagerOptions) Sanitize() {
+	if l.Domain == "" {
+		l.Domain = "tcp"
+	}
+
+	if l.MaxConcurrent <= 0 {
+		l.MaxConcurrent = 1
+	}
+}
+
+func NewConnectionManager(options ConnectionManagerOptions) (*ConnectionManager, error) {
+	options.Sanitize()
+
+	l, err := net.Listen(options.Domain, options.Addr)
+	if err != nil {
+		return nil, errors.Wrap(err)
+	}
+
+	return &ConnectionManager{
+		connectionLimit: semaphore.NewWeighted(options.MaxConcurrent),
+		l:               l,
+	}, nil
+}
+
+// Like net.Listener, but supporting an upper bound
+// on concurrent connections. The returned connections
+// also track read/write traffic.
+type ConnectionManager struct {
+	connectionLimit *semaphore.Weighted
+	l               net.Listener
+}
+
+func (cm *ConnectionManager) Accept() (net.Conn, error) {
+
+	err := cm.connectionLimit.Acquire(context.TODO(), 1)
+	if err != nil {
+		return nil, errors.Wrap(err)
+	}
+
+	conn, err := cm.l.Accept()
+	if err != nil {
+		return nil, errors.Wrap(err)
+	}
+
+	return &Connection{
+		manager: cm,
+		Conn:    conn,
+	}, nil
+}
+
+func (cm *ConnectionManager) Release(c *Connection) {
+	cm.connectionLimit.Release(1)
+}
+
+func (cm *ConnectionManager) Close() error {
+	return cm.l.Close()
+}
+
+func (cm *ConnectionManager) Addr() net.Addr {
+	return cm.l.Addr()
+}
+
+type Connection struct {
+	manager    *ConnectionManager
+	Conn       net.Conn
+	readCount  uint64
+	writeCount uint64
+}
+
+// Safe for concurrent use
+func (conn *Connection) ReadCount() uint64 {
+	return atomic.LoadUint64(&conn.readCount)
+}
+
+// Safe for concurrent use
+func (conn *Connection) WriteCount() uint64 {
+	return atomic.LoadUint64(&conn.writeCount)
+}
+
+func (conn *Connection) Read(buf []byte) (int, error) {
+	n, err := conn.Conn.Read(buf)
+	atomic.AddUint64(&conn.readCount, uint64(n))
+	return n, err
+}
+
+func (conn *Connection) Write(buf []byte) (int, error) {
+	n, err := conn.Conn.Write(buf)
+	atomic.AddUint64(&conn.writeCount, uint64(n))
+	return n, err
+}
+
+func (conn *Connection) Close() error {
+	conn.manager.Release(conn)
+	return conn.Conn.Close()
+}
+
+func (conn *Connection) LocalAddr() net.Addr {
+	return conn.Conn.LocalAddr()
+}
+
+func (conn *Connection) RemoteAddr() net.Addr {
+	return conn.Conn.RemoteAddr()
+}
+
+func (conn *Connection) SetDeadline(t time.Time) error {
+	return conn.Conn.SetDeadline(t)
+}
+
+func (conn *Connection) SetReadDeadline(t time.Time) error {
+	return conn.Conn.SetReadDeadline(t)
+}
+
+func (conn *Connection) SetWriteDeadline(t time.Time) error {
+	return conn.Conn.SetWriteDeadline(t)
+}
