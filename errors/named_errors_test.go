69f63d44dbd80d215d7f53930b0f1c12e5419fd6
Andrew Chambers <andrewchambers@fastmail.com>
2018-10-23 12:59:27 +1300

    Add simple sanity check.

--- errors/named_errors_test.go
+++ errors/named_errors_test.go
@@ -1,5 +1,7 @@
 package errors
 
+import "testing"
+
 const SampleErrorCode = 0xc1f42d98766266aa
 
 var SampleErr = NewNamed("TestError", SampleErrorCode)
@@ -10,5 +12,16 @@ func ExampleNamedError() {
 	}
 	if FromCode("fallback msg", SampleErrorCode) != SampleErr {
 		panic("bug")
+	}
+}
+
+// Mainly a sanity check of my understanding of interface comparisons.
+func TestCompare(t *testing.T) {
+	var err error
+
+	err = SampleErr
+
+	if err != SampleErr {
+		t.Fail()
 	}
 }
